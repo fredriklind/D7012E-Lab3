395af68af107a9dff3f85741b88737b95714550f
Fredrik Lind <fredrik@editerat.se>
2014-05-20 14:50:58 +0200

    Done. Now.

--- lab3s.pl
+++ lab3s.pl
@@ -1,18 +1,20 @@
+% Unsafe if Fox and Goose or Goose and beans are left alone.
 safe([M,F,G,B]):- not((M\=F,F=G);(M\=G,G=B)).
 
-move([w,w,G,B],[e,e,G,B],f,e). % Fox 	east
-move([w,F,w,B],[e,F,e,B],g,e). % Goose 	east
-move([w,F,G,w],[e,F,G,e],b,e). % Beans 	east
-move([e,e,G,B],[w,w,G,B],f,w). % Fox 	west
-move([e,F,e,B],[w,F,w,B],g,w). % Goose 	west
-move([e,F,G,e],[w,F,G,w],b,w). % Beans 	west
-move([w|T],[e|T],m,e). 		   % Man 	east
-move([e|T],[w|T],m,w). 		   % Man 	west
+% Go from west to east, and vice versa.
+i(e,w).
+i(w,e).
 
+% Move either Fox, Goose or man either west or east.
+move([X,X,G,B],[Y,Y,G,B],f,Y):- i(X,Y). % Fox
+move([X,F,X,B],[Y,F,Y,B],g,Y):- i(X,Y). % Goose
+move([X,F,G,X],[Y,F,G,Y],b,Y):- i(X,Y). % Beans
+move([X|T],[Y|T],m,Y):- 		i(X,Y). % Man
+
+% Goal, everything at Destination
 solvefgb([D,D,D,D],D,_,[]).
-solvefgb(State,Dest,N,[(M,D)|Trace]):-
+solvefgb(State,Dest,N,[(Moved,To)|Trace]):-
 	N>0,
-	move(State,NewState,M,D),
+	move(State,NewState,Moved,To),
 	safe(NewState),
-	N1 is N-1,
-	solvefgb(NewState,Dest,N1,Trace).
+	solvefgb(NewState,Dest,N-1,Trace).
